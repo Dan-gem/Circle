9e4836ed65631d044db8e941f4fdc6cbc97c7aac
Dan-gem <Dan_dan_gen@mail.ru>
2017-01-23 18:26:08 +0600

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,11 @@
+public class Circle {
+    public static void main(String[] args) {
+        final double PI = 3.1414;
+        int r = 7;
+        double s = PI * Math.pow (r,2);
+        System.out.println (s);
+
+
+
+    }
+}
